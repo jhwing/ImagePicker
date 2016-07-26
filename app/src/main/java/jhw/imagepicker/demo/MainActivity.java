ee012706a163aa5af9e12b406e8cb63e3f631e27
jihongwen <jhwing@163.com>
2016-07-26 15:26:55 +0800

    android 6.0 动态权限

--- app/src/main/java/jhw/imagepicker/demo/MainActivity.java
+++ app/src/main/java/jhw/imagepicker/demo/MainActivity.java
@@ -1,6 +1,11 @@
 package jhw.imagepicker.demo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +13,16 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.lang.ref.SoftReference;
+
 import jhw.imagepicker.ImagePicker;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 100;
+    private static final int REQUEST_READ_EXTERNAL_STORAGE_CODE = 101;
+
+    private SoftReference<View> requestView;
 
     ImageView image;
 
@@ -20,8 +30,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         image = (ImageView) findViewById(R.id.image);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startImagePicker(requestView.get());
+            }
+        }
     }
 
     @Override
@@ -37,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startImagePicker(View view) {
-        ImagePicker.startImagePicker(this, REQUEST_CODE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_EXTERNAL_STORAGE_CODE);
+            requestView = new SoftReference<>(view);
+        } else {
+            ImagePicker.startImagePicker(this, REQUEST_CODE);
+        }
     }
 }
